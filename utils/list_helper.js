73e974e0aef27a5fb73a171d6d7d9eefef1d88a0
topiaspe <topiaspe@metropolia.fi>
2022-04-02 13:02:47 +0300

    exercise 4.4 helpers and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1;
+}
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    const likes = blogs.reduce(
+        (prev, curr) => prev + curr.likes, 0
+    )
+    console.log('TotalLikes ', likes)
+    return likes
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
